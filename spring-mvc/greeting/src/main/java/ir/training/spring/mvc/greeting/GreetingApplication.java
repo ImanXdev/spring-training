3eac0be26d0996fc866c86306590d87909877511
Iman Baharlooei <imanbaharlooei@gmail.com>
2020-05-03 00:27:19 +0430

    - mvc training

---
+++ spring-mvc/greeting/src/main/java/ir/training/spring/mvc/greeting/GreetingApplication.java
@@ -0,0 +1,13 @@
+package ir.training.spring.mvc.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingApplication.class, args);
+	}
+
+}
